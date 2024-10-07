d46c62cd7cd2a6457408cb30256ddd2ca4e1e105
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-10-07 15:34:14 +0300

    Added GetDTO models

---
+++ DatabaseModel/DTO/GetDTO/DocumentGetDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseModel.DTO
+{
+    public class DocumentDTO
+    {
+        public int Id { get; set; }
+        public DateTime CreationTime { get; set; }
+        public int WarehouseEmployeeId { get; set; }        
+        public int DocumentTypeId { get; set; }       
+        public int CounterpartyId { get; set; }       
+    }
+}
