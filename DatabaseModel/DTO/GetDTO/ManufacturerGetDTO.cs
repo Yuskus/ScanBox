6e703c4807d5b89f057d01e6d46badeda594d205
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-10-07 15:34:14 +0300

    Added GetDTO models

---
+++ DatabaseModel/DTO/GetDTO/ManufacturerGetDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseModel.DTO.GetDTO
+{
+    internal class ManufacturerGetDTO
+    {
+        public int Id { get; set; }
+        public int CounterpartyId { get; set; }
+    }
+}
