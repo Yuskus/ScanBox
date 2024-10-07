336b5fb9a7d03a87bb694134f5bfc05833fbeaa4
Yuskus <yuskus@yandex.ru>
2024-10-07 22:01:58 +0300

    Authorization addedgit status! Wowgit status

---
+++ DatabaseModel/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace DatabaseModel
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string PasswordHash { get; set; }
+        public int Role { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ ScanBoxWebApi/Abstractions/ITokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using DatabaseModel.DTO;
+
+namespace ScanBoxWebApi.Abstractions
+{
+    public interface ITokenGenerator
+    {
+        string GetToken(UserDTO userDTO);
+    }
+}
