dec1218d8d741104f3a0be7ac38678e76bda36f2
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 22:18:07 +0300

    Added methods for IIndividualRepository

--- ScanBoxWebApi/Abstractions/IIndividualRepositoty.cs
+++ ScanBoxWebApi/Abstractions/IIndividualRepositoty.cs
@@ -1,6 +1,13 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface IIndividualRepositoty : IRepository
     {
+        public int AddIndividual(IndividualPostDTO individualPostDTO);
+        public int PutIndividual(IndividualPostDTO individualPutDTO);
+        public int DelIndividual(int individualId);
+        public IEnumerable<IndividualGetDTO> GetIndividual();
     }
 }
