a95fd7b4720ad31bea78ae86407383d058b3080b
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 23:49:28 +0300

    Added ShipmentGetDTO and ShipmentPostDTO

---
+++ DatabaseModel/DTO/PostDTO/ShipmentPostDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseModel.DTO.PostDTO
+{
+    internal class ShipmentPostDTO
+    {
+        public int DocumentId { get; set; }
+        public int ProductUnitId { get; set; }
+    }
+}
