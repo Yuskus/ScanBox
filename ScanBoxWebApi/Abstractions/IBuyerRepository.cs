010e09a1d987478f5a313aac03b64a46ae32772b
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 22:04:04 +0300

    Added methods for ICounterpartyReository

--- ScanBoxWebApi/Abstractions/IBuyerRepository.cs
+++ ScanBoxWebApi/Abstractions/IBuyerRepository.cs
@@ -7,8 +7,9 @@ namespace ScanBoxWebApi.Abstractions
     public interface IBuyerRepository : IRepository
     {
         public int AddBuyer(BuyerPostDTO buyerPostDTO);
+        public int PutBuyer(BuyerPostDTO buyerPutDTO);
         public int DelBuyer(int buyerId);
         public IEnumerable <BuyerGetDTO> GetBuyers();
-
+        
     }
 }
