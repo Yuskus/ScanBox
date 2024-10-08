92ea37e498ee4da88e28ff45a29ecfef2c7495c5
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 22:10:29 +0300

    Added methods for IDocumentRepository

--- ScanBoxWebApi/Abstractions/IDocumentRepository.cs
+++ ScanBoxWebApi/Abstractions/IDocumentRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface IDocumentRepository : IRepository
     {
+        public int AddDocument(DocumentPostDTO documentPostDTO);
+        public int PutDocument(DocumentPostDTO documentPutDTO);
+        public int DeleteDocument(int documentId);
+        public IEnumerable<DocumentGetDTO> GetDocuments();
+
     }
 }
