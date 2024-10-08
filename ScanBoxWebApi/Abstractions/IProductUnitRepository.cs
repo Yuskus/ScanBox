77a3f17954f48882351d3cc52c54354a01e59350
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 23:12:10 +0300

    Added methods for IProductUnitRepository

--- ScanBoxWebApi/Abstractions/IProductUnitRepository.cs
+++ ScanBoxWebApi/Abstractions/IProductUnitRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface IProductUnitRepository : IRepository
     {
+        public int AddProductUnit(ProductUnitPostDTO productUnitPostDTO);
+        public int PutProductUnit(ProductUnitPostDTO productUnitPutDTO);
+        public int DelProductUnit(int productUnitId);
+        public IEnumerable<ProductUnitGetDTO> GetProductUnits();
     }
 }
