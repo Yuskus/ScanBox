d6f16e7a57c3120f16307ede0ee400ad2779d486
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 22:20:59 +0300

    Added methods for IJobTitelRepository

--- ScanBoxWebApi/Abstractions/IJobTitelRepository.cs
+++ ScanBoxWebApi/Abstractions/IJobTitelRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface IJobTitelRepository : IRepository
     {
+        public int AddJobTitel(JobTitelPostDTO jobTitelPostDTO);
+        public int PutJobTitel(JobTitelPostDTO jobTitelPutDTO);
+        public int DelJobTitel(int jobTitelId);
+        public IEnumerable<JobTitelGetDTO> GetJobTitel();  
     }
 }
