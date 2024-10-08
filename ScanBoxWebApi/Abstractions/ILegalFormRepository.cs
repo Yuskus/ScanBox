0a33467c44321f74c291147d3afd3e3d739d6bd5
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 22:31:44 +0300

    Added methods for ILegalFormRepository

--- ScanBoxWebApi/Abstractions/ILegalFormRepository.cs
+++ ScanBoxWebApi/Abstractions/ILegalFormRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface ILegalFormRepository : IRepository
     {
+        public int AddLegalForm(LegalFormPostDTO legalFormPostDTO);
+        public int PutLegalForm(LegalFormPostDTO legalFormPutDTO);
+        public int DelLegalForm(int legalFormId);
+        public IEnumerable<LegalEntityGetDTO> GetLegalEntity();
     }
 }
