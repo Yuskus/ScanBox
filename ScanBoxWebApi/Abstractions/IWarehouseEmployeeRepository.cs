d7e1caed1293cd47ab3bc76518e311046bfd645a
Yuriy <yuriy_vm@inbox.ru>
2024-10-09 00:02:43 +0300

    Added methods for IWarehouseEmployeeRepository

--- ScanBoxWebApi/Abstractions/IWarehouseEmployeeRepository.cs
+++ ScanBoxWebApi/Abstractions/IWarehouseEmployeeRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface IWarehouseEmployeeRepository : IDeleteRepository
     {
+        public int AddWarehouseEmployee(WarehouseEmployeePostDTO warehouseEmployeePostDTO);
+        public int PutWarehouseEmployee(WarehouseEmployeePostDTO warehouseEmployeePutDTO);
+        public IEnumerable<WarehouseEmployeeGetDTO> GetWarehouseEmployeesPosts();
     }
 }
