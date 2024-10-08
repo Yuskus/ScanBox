472723328a755d67d7425c2f5b1cd4781d1ee462
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 22:42:55 +0300

    Added methods for IMovementHistoryRepository

--- ScanBoxWebApi/Abstractions/IMovementHistoryRepository.cs
+++ ScanBoxWebApi/Abstractions/IMovementHistoryRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface IMovementHistoryRepository : IRepository
     {
+        public int AddMovementHistory(MovementHistoryPostDTO movementHistoryPostDTO);
+        public int PutMovementHistory(MovementHistoryPostDTO movementHistoryPuttDTO);
+        public int DelMovementHistory(int movementHistoryId);
+        public IEnumerable<MovementHistoryGetDTO> GetMovementHistory();
     }
 }
