f2e997c6e424145061ee524f7973a4e0f2b51737
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 22:40:11 +0300

    Added methods for IManufacturerRepository

--- ScanBoxWebApi/Abstractions/IManufacturerRepository.cs
+++ ScanBoxWebApi/Abstractions/IManufacturerRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface IManufacturerRepository : IRepository
     {
+        public int AddManufacturer(ManufacturerPostDTO manufacturerPostDTO);
+        public int PutManufacturer(ManufacturerPostDTO manufacturerPutDTO);
+        public int DelManufacturer(int manufacturerId);
+        public IEnumerable<ManufacturerGetDTO> GetManufacturers();
     }
 }
