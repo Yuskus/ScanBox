28d1c2dd974615ecafcad759cd1669b66c30b973
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 23:04:30 +0300

    Added methods for IProductCategoryRepository

--- ScanBoxWebApi/Abstractions/IProductCategoryRepository.cs
+++ ScanBoxWebApi/Abstractions/IProductCategoryRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface IProductCategoryRepository : IRepository
     {
+        public int AddProductCategory(ProductCategoryPostDTO productCategoryPostDTO);
+        public int PutProductCategory(ProductCategoryPostDTO productCategoryPutDTO);
+        public int DelProductCategory(int productCategoryId);
+        public IEnumerable<ProductCategoryGetDTO> GetProductCategories();
     }
 }
