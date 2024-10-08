603c6712f0401bf0667c89a2acc1583580521697
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 22:04:04 +0300

    Added methods for ICounterpartyReository

--- ScanBoxWebApi/Abstractions/ICounterpartyRepository.cs
+++ ScanBoxWebApi/Abstractions/ICounterpartyRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface ICounterpartyRepository : IRepository
     {
+        public int AddCounterparty(CounterpartyPostDTO counterpartyPostDTO);
+        public int PutCounterparty(CounterpartyPostDTO counterpartyPutDTO);
+        public int DeleteCounterparty(int counterpartyId);
+        public IEnumerable<CounterpartyGetDTO> counterpartiesGetDTO();
+        
     }
 }
--- ScanBoxWebApi/Abstractions/ICounterpartyTypeRepository.cs
+++ ScanBoxWebApi/Abstractions/ICounterpartyTypeRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface ICounterpartyTypeRepository : IRepository
     {
+        public int AddCounterpartyType (CounterpartyTypePostDTO counterpartyTypePostDTO);
+        public int PutCounterpartyType(CounterpartyTypePostDTO counterpartyTypePutDTO);
+        public int DeleteCounterpartyType(int counterpartyTypeId);
+        public IEnumerable<CounterpartyTypeGetDTO> counterpartiesTypeGetDTO();        
     }
 }
