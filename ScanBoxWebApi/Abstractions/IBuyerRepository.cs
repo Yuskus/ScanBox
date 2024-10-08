28f6806416f64a3cd7f30f0ffdefe7e19e2fd97f
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 21:37:08 +0300

    Added methods for IBuyerRepository

--- ScanBoxWebApi/Abstractions/IBuyerRepository.cs
+++ ScanBoxWebApi/Abstractions/IBuyerRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel;
+using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface IBuyerRepository : IRepository
     {
+        public int AddBuyer(BuyerPostDTO buyerPostDTO);
+        public int DelBuyer(int buyerId);
+        public IEnumerable <BuyerGetDTO> GetBuyers();
+
     }
 }
