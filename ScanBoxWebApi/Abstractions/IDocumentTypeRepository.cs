a8b022ce0c5903f3669cbc3b2e87613006d8e46b
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 22:20:59 +0300

    Added methods for IJobTitelRepository

--- ScanBoxWebApi/Abstractions/IDocumentTypeRepository.cs
+++ ScanBoxWebApi/Abstractions/IDocumentTypeRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface IDocumentTypeRepository : IRepository
     {
+        public int AddDocumentType(DocumentTypePostDTO documentTypePostDTO);
+        public int PutDocumentType(DocumentTypePostDTO documentTypePutDTO);
+        public int DelDocumentType(int documentTypeId);
+        public IEnumerable<DocumentTypeGetDTO> GetDocumentTypes();
     }
 }
