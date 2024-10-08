8e34c7a9a38c054cb3b2d4924a2a06f5eb9c4e67
Yuriy <yuriy_vm@inbox.ru>
2024-10-08 23:06:54 +0300

    Added methods for IPricesRepository

--- ScanBoxWebApi/Abstractions/IPricesRepository.cs
+++ ScanBoxWebApi/Abstractions/IPricesRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ScanBoxWebApi.Abstractions
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+
+namespace ScanBoxWebApi.Abstractions
 {
     public interface IPricesRepository : IRepository
     {
+        public int AddPrices(PricesPostDTO pricesPostDTO);
+        public int PutPrices(PricesPostDTO pricesPutDTO);
+        public int DelPrices(int pricesId);
+        public IEnumerable<PricesGetDTO> GetPrices();
     }
 }
