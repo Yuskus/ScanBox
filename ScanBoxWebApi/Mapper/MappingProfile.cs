c26a3d1359ebccdc717c27adf1293fad8c563cd8
Yuskus <yuskus@yandex.ru>
2024-10-08 21:27:59 +0300

    first try to add hashing

---
+++ ScanBoxWebApi/Mapper/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DatabaseModel;
+using DatabaseModel.DTO;
+
+namespace ScanBoxWebApi.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // чтобы из сущности пользователя создать объект прав пользователя
+            CreateMap<UserEntity, UserDTO>();
+
+            // чтобы заполнить 
+            CreateMap<LoginFormDTO, UserEntity>();
+        }
+    }
+}
