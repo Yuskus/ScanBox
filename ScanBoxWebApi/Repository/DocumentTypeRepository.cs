6cac2070803fcb2245762a63a160311ab5dfc1d6
Yuriy <yuriy_vm@inbox.ru>
2024-10-10 23:11:08 +0300

    modified DocumentTypeRepository

--- ScanBoxWebApi/Repository/DocumentTypeRepository.cs
+++ ScanBoxWebApi/Repository/DocumentTypeRepository.cs
@@ -1,29 +1,66 @@
-﻿using DatabaseModel.DTO.GetDTO;
+﻿using AutoMapper;
+using DatabaseModel;
+using DatabaseModel.Context;
+using DatabaseModel.DTO.GetDTO;
 using DatabaseModel.DTO.PostDTO;
 using ScanBoxWebApi.Abstractions;
+using System.Reflection.Metadata;
 
 namespace ScanBoxWebApi.Repository
 {
-    public class DocumentTypeRepository : IDocumentTypeRepository
+    public class DocumentTypeRepository : ICrudMethodRepository<DocumentTypeGetDTO, DocumentTypePostDTO>
     {
-        public int AddDocumentType(DocumentTypePostDTO documentTypePostDTO)
+        public readonly ScanBoxDbContext _context;
+        public readonly IMapper _mapper;
+
+        public DocumentTypeRepository(ScanBoxDbContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _mapper = mapper;
         }
 
-        public int Delete(int Id)
+        public int Create(DocumentTypePostDTO documentTypeDto)
         {
-            throw new NotImplementedException();
+            var documentTypeEntity = _context.DocumentType.FirstOrDefault(x => x.DoctypeName == documentTypeDto.DoctypeName);
+            if (documentTypeEntity == null)
+            {
+                documentTypeEntity = _mapper.Map<DocumentTypeEntity>(documentTypeDto);
+                _context.Add(documentTypeEntity);
+                _context.SaveChanges();
+            }
+            return documentTypeEntity.Id;
         }
 
-        public IEnumerable<DocumentTypeGetDTO> GetDocumentTypes()
+        public int Delete(int documentTypeId)
         {
-            throw new NotImplementedException();
+            var documentTypeEntity = _context.DocumentType.FirstOrDefault(x => x.Id == documentTypeId);
+            if (documentTypeEntity != null)
+            {
+                _context.Remove(documentTypeEntity);
+                _context.SaveChanges();
+                return documentTypeEntity.Id;
+            }
+            return -1;
         }
 
-        public int PutDocumentType(DocumentTypePostDTO documentTypePutDTO)
+        public IEnumerable<DocumentTypeGetDTO> GetElemetsList()
         {
-            throw new NotImplementedException();
+            var documentTypeEntity = _context.DocumentType.Select(x => _mapper.Map<DocumentTypeGetDTO>(x));
+            return documentTypeEntity;
+        }
+
+        public int Update(DocumentTypeGetDTO documentTypeDto)
+        {
+            var documentTypeEntity = _context.DocumentType.FirstOrDefault(x => x.Id == documentTypeDto.Id);
+            if (documentTypeEntity != null)
+            {
+                documentTypeEntity.DoctypeName = documentTypeDto.DoctypeName;
+                documentTypeEntity.Description = documentTypeDto.Description;
+                
+                _context.SaveChanges();
+                return documentTypeEntity.Id;
+            }
+            return -1;
         }
     }
 }
