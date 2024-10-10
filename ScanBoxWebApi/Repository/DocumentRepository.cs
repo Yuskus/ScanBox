35dfdeaa7589244ebd1aea88fbf50359f79c28c8
Yuriy <yuriy_vm@inbox.ru>
2024-10-10 22:19:39 +0300

    modified: ScanBoxWebApi/Repository/DocumentRepository

--- ScanBoxWebApi/Repository/DocumentRepository.cs
+++ ScanBoxWebApi/Repository/DocumentRepository.cs
@@ -1,29 +1,69 @@
-﻿using DatabaseModel.DTO.GetDTO;
+﻿using AutoMapper;
+using DatabaseModel;
+using DatabaseModel.Context;
+using DatabaseModel.DTO.GetDTO;
 using DatabaseModel.DTO.PostDTO;
 using ScanBoxWebApi.Abstractions;
 
 namespace ScanBoxWebApi.Repository
 {
-    public class DocumentRepository : IDocumentRepository
+    public class DocumentRepository : ICrudMethodRepository<DocumentGetDTO, DocumentPostDTO>
     {
-        public int AddDocument(DocumentPostDTO documentPostDTO)
+        public readonly ScanBoxDbContext _context;
+        public readonly IMapper _mapper;
+
+        public DocumentRepository(ScanBoxDbContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _mapper = mapper;
         }
 
-        public int Delete(int Id)
+        public int Create(DocumentPostDTO documentDto)
         {
-            throw new NotImplementedException();
+            var documentEntity = _context.Document.FirstOrDefault(x => x.Number == documentDto.Number);
+            if (documentEntity == null)
+            {
+                documentEntity = _mapper.Map<DocumentEntity>(documentDto);
+                _context.Add(documentEntity);
+                _context.SaveChanges();                
+            }
+            return documentEntity.Id;
+
         }
 
-        public IEnumerable<DocumentGetDTO> GetDocuments()
+        public int Delete(int documentId)
         {
-            throw new NotImplementedException();
+            var documentEntity = _context.Document.FirstOrDefault(x => x.Id == documentId);
+            if (documentEntity != null)
+            {
+                _context.Remove(documentEntity);
+                _context.SaveChanges();
+                return documentEntity.Id;
+            }
+            return -1;
         }
 
-        public int PutDocument(DocumentPostDTO documentPutDTO)
+        public IEnumerable<DocumentGetDTO> GetElemetsList()
         {
-            throw new NotImplementedException();
+            var documentEntity = _context.Document.Select(x => _mapper.Map<DocumentGetDTO>(x));
+            return documentEntity;
+        }
+
+        public int Update(DocumentGetDTO documentDto)
+        {
+            var documentEntity = _context.Document.FirstOrDefault(x => x.Id == documentDto.Id);
+            if (documentEntity != null)
+            {
+                documentEntity.Number = documentDto.Number;
+                documentEntity.CreationTime = documentDto.CreationTime;
+                documentEntity.WarehouseEmployeeId = documentDto.WarehouseEmployeeId;
+                documentEntity.DocumentTypeId = documentDto.DocumentTypeId;
+                documentEntity.CounterpartyId = documentDto.CounterpartyId;
+                
+                _context.SaveChanges();
+                return documentEntity.Id;
+            }
+            return -1;
         }
     }
 }
