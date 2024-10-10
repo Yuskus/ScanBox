09ab540ba00bbacc903aea8a7dc15644dd5a7b65
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-10-10 16:30:44 +0300

    Added methods 'Delete' and 'Add' to CounterpartyRepository

--- ScanBoxWebApi/Repository/CounterpartyRepository.cs
+++ ScanBoxWebApi/Repository/CounterpartyRepository.cs
@@ -1,27 +1,50 @@
-﻿using DatabaseModel.DTO.GetDTO;
+﻿using AutoMapper;
+using DatabaseModel;
+using DatabaseModel.Context;
+using DatabaseModel.DTO.GetDTO;
 using DatabaseModel.DTO.PostDTO;
 using ScanBoxWebApi.Abstractions;
 
 namespace ScanBoxWebApi.Repository
 {
-    public class CounterpartyRepository : ICounterpartyRepository
+    public class CounterpartyRepository : ICrudMethodRepository<CounterpartyGetDTO, CounterpartyPostDTO>
     {
-        public int AddCounterparty(CounterpartyPostDTO counterpartyPostDTO)
+        public readonly ScanBoxDbContext _context;
+        public readonly IMapper _mapper;
+
+        public CounterpartyRepository(ScanBoxDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;            
+        }
+
+        public int Create(CounterpartyPostDTO counterpartyDto)
+        {
+            var counterpartyEntity = _mapper.Map<CounterpartyEntity>(counterpartyDto);
+            _context.Add(counterpartyEntity);
+            _context.SaveChanges();
+            return counterpartyEntity.Id;
+        }
+
+        public int Delete(int counterpartyId)
+        {
+            var counterpartyEntity = _context.Counterparties.FirstOrDefault(x => x.Id == counterpartyId);
+
+            if (counterpartyEntity != null)
+            {
+                _context.Remove(counterpartyEntity);
+                _context.SaveChanges();
+                return counterpartyEntity.Id;
+            }
+            return -1;
+        }
+
+        public IEnumerable<CounterpartyGetDTO> GetElemetsList()
         {
             throw new NotImplementedException();
         }
 
-        public int Delete(int Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<CounterpartyGetDTO> GetCounterparties()
-        {
-            throw new NotImplementedException();
-        }
-
-        public int PutCounterparty(CounterpartyPostDTO counterpartyPutDTO)
+        public int Update(CounterpartyPostDTO counterpartyDto)
         {
             throw new NotImplementedException();
         }
