a01d24a16ca8e9d759732fa64c08af46409277e0
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-10-10 17:27:33 +0300

    Added method 'Create' to CounterpartyTypeRepository

--- ScanBoxWebApi/Repository/CounterpartyTypeRepository.cs
+++ ScanBoxWebApi/Repository/CounterpartyTypeRepository.cs
@@ -1,14 +1,35 @@
-﻿using DatabaseModel.DTO.GetDTO;
+﻿using AutoMapper;
+using DatabaseModel;
+using DatabaseModel.Context;
+using DatabaseModel.DTO.GetDTO;
 using DatabaseModel.DTO.PostDTO;
 using ScanBoxWebApi.Abstractions;
 
 namespace ScanBoxWebApi.Repository
 {
-    public class CounterpartyTypeRepository : ICounterpartyTypeRepository
+    public class CounterpartyTypeRepository : ICrudMethodRepository<CounterpartyTypeGetDTO, CounterpartyTypePostDTO>
     {
-        public int AddCounterpartyType(CounterpartyTypePostDTO counterpartyTypePostDTO)
+        public readonly ScanBoxDbContext _context;
+        public readonly IMapper _mapper;
+
+        public CounterpartyTypeRepository(ScanBoxDbContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public int Create(CounterpartyTypePostDTO counterpartyTypeDto)
+        {
+            var counterpartyTypeEntity = _context.CounterpartiesTypes.FirstOrDefault(x => x.TypeName.Equals(counterpartyTypeDto.TypeName));
+
+            if (counterpartyTypeEntity == null)
+            {
+                counterpartyTypeEntity = _mapper.Map<CounterpartyTypeEntity>(counterpartyTypeDto);
+                _context.Add(counterpartyTypeEntity);
+                _context.SaveChanges();
+                return counterpartyTypeEntity.Id;
+            }
+            return -1;
         }
 
         public int Delete(int Id)
@@ -16,12 +37,12 @@ namespace ScanBoxWebApi.Repository
             throw new NotImplementedException();
         }
 
-        public IEnumerable<CounterpartyTypeGetDTO> GetCounterpartiesType()
+        public IEnumerable<CounterpartyTypeGetDTO> GetElemetsList()
         {
             throw new NotImplementedException();
         }
 
-        public int PutCounterpartyType(CounterpartyTypePostDTO counterpartyTypePutDTO)
+        public int Update(CounterpartyTypeGetDTO dto)
         {
             throw new NotImplementedException();
         }
