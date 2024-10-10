0a3697a0fa6716fa49ae48591a2abb1f52f2d34c
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-10-10 13:10:45 +0300

    Added method 'Update' to BuyerRepository

--- ScanBoxWebApi/Repository/BuyerRepository.cs
+++ ScanBoxWebApi/Repository/BuyerRepository.cs
@@ -15,7 +15,7 @@ namespace ScanBoxWebApi.Repository
         public BuyerRepository(ScanBoxDbContext context, IMapper mapper)
         {
             _context = context;
-            _mapper = mapper;            
+            _mapper = mapper;
         }
 
         public int Create(BuyerPostDTO buyerDTO)
@@ -28,12 +28,12 @@ namespace ScanBoxWebApi.Repository
                 _context.Add(buyerEntity);
                 _context.SaveChanges();
             }
-            return buyerEntity.Id;            
+            return buyerEntity.Id;
         }
 
         public int Delete(int buyerId)
         {
-            var buyerEntity = _context.Buyers.FirstOrDefault(x => x.Id==buyerId);
+            var buyerEntity = _context.Buyers.FirstOrDefault(x => x.Id == buyerId);
             if (buyerEntity != null)
             {
                 _context.Remove(buyerEntity);
@@ -46,12 +46,23 @@ namespace ScanBoxWebApi.Repository
         public IEnumerable<BuyerGetDTO> GetElemetsList()
         {
             var buyerEntities = _context.Buyers.Select(x => _mapper.Map<BuyerGetDTO>(x));
-            return buyerEntities;            
+            return buyerEntities;
         }
 
-        public int Update(BuyerPostDTO dto)
+        public int Update(BuyerPostDTO buyerDto)
         {
-            throw new NotImplementedException();
+            var buyerEntity = _context.Buyers.FirstOrDefault(x => x.CounterpartyId == buyerDto.CounterpartyId);
+            if (buyerEntity == null)
+            {
+                buyerEntity = _mapper.Map<BuyerEntity>(buyerDto);
+                _context.Add(buyerEntity);
+            }
+            else
+            {
+               buyerEntity.CounterpartyId = buyerDto.CounterpartyId;
+            }
+            _context.SaveChanges();
+            return buyerEntity.Id;
         }
     }
 }
