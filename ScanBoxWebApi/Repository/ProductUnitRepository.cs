96a23daf7ca115eb5419a285f1dc839217913554
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-10-11 17:33:39 +0300

    Modified ProductUnitRepository

--- ScanBoxWebApi/Repository/ProductUnitRepository.cs
+++ ScanBoxWebApi/Repository/ProductUnitRepository.cs
@@ -1,29 +1,71 @@
-﻿using DatabaseModel.DTO.GetDTO;
+﻿using AutoMapper;
+using DatabaseModel;
+using DatabaseModel.Context;
+using DatabaseModel.DTO.GetDTO;
 using DatabaseModel.DTO.PostDTO;
 using ScanBoxWebApi.Abstractions;
 
 namespace ScanBoxWebApi.Repository
 {
-    public class ProductUnitRepository : IProductUnitRepository
+    public class ProductUnitRepository : ICrudMethodRepository<ProductUnitGetDTO,ProductUnitPostDTO>
     {
-        public int AddProductUnit(ProductUnitPostDTO productUnitPostDTO)
+        public readonly ScanBoxDbContext _context;
+        public readonly IMapper _mapper;
+
+        public ProductUnitRepository(ScanBoxDbContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _mapper = mapper;
         }
 
-        public int Delete(int Id)
+        public int Create(ProductUnitPostDTO productUnitDto)
         {
-            throw new NotImplementedException();
+            var productUnitEntity = _context.ProductUnits.FirstOrDefault(x => x.UniqueBarcode.Equals(productUnitDto.UniqueBarcode));
+            if (productUnitEntity is null)
+            {
+                productUnitEntity = _mapper.Map<ProductUnitEntity>(productUnitDto);
+                _context.Add(productUnitEntity);
+                _context.SaveChanges();
+            }
+            return productUnitEntity.Id;
         }
 
-        public IEnumerable<ProductUnitGetDTO> GetProductUnits()
+        public int Delete(int productUnitId)
         {
-            throw new NotImplementedException();
+            var productUnitEntity = _context.ProductUnits.FirstOrDefault(x => x.Id == productUnitId);
+
+            int result = -1;
+            if (productUnitEntity is not null)
+            {
+                result = productUnitEntity.Id;
+                _context.Remove(productUnitEntity);
+                _context.SaveChanges();
+            }
+            return result;
         }
 
-        public int PutProductUnit(ProductUnitPostDTO productUnitPutDTO)
+        public IEnumerable<ProductUnitGetDTO> GetElemetsList()
         {
-            throw new NotImplementedException();
+            var productUnitEntity = _context.ProductUnits.Select(x => _mapper.Map<ProductUnitGetDTO>(x));
+            return productUnitEntity;
+        }
+
+        public int Update(ProductUnitGetDTO productUnitDto)
+        {
+            var productUnitEntity = _context.ProductUnits.FirstOrDefault(x => x.Id == productUnitDto.Id);
+
+            if (productUnitEntity is not null)
+            {
+                productUnitEntity.UniqueBarcode = productUnitDto.UniqueBarcode;
+                productUnitEntity.ProductionDate = productUnitDto.ProductionDate;
+                productUnitEntity.RealizationPrice = productUnitDto.RealizationPrice;
+                productUnitEntity.ProductTypeId = productUnitDto.ProductTypeId;
+                productUnitEntity.SupplierId = productUnitDto.SupplierId;               
+
+                _context.SaveChanges();
+                return productUnitEntity.Id;
+            }
+            return -1;
         }
     }
 }
