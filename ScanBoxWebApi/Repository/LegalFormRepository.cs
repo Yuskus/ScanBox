50778fa3f71506227960282e2af3a06d25abe827
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-10-11 12:31:20 +0300

    Modified LegalFormRepository

--- ScanBoxWebApi/Repository/LegalFormRepository.cs
+++ ScanBoxWebApi/Repository/LegalFormRepository.cs
@@ -1,29 +1,65 @@
-﻿using DatabaseModel.DTO.GetDTO;
+﻿using AutoMapper;
+using DatabaseModel;
+using DatabaseModel.Context;
+using DatabaseModel.DTO.GetDTO;
 using DatabaseModel.DTO.PostDTO;
 using ScanBoxWebApi.Abstractions;
 
 namespace ScanBoxWebApi.Repository
 {
-    public class LegalFormRepository : ILegalFormRepository
+    public class LegalFormRepository : ICrudMethodRepository<LegalFormGetDTO, LegalFormPostDTO>
     {
-        public int AddLegalForm(LegalFormPostDTO legalFormPostDTO)
+        public readonly ScanBoxDbContext _context;
+        public readonly IMapper _mapper;
+
+        public LegalFormRepository( ScanBoxDbContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _mapper = mapper;
         }
 
-        public int Delete(int Id)
+        public int Create(LegalFormPostDTO legalFormDto)
         {
-            throw new NotImplementedException();
+            var legalFormEntity = _context.LegalForms.FirstOrDefault(x => x.LegalFormName == legalFormDto.LegalFormName);
+            if (legalFormEntity == null)
+            {
+                legalFormEntity = _mapper.Map<LegalFormEntity>(legalFormDto);
+                _context.Add(legalFormEntity);
+                _context.SaveChanges();
+            }
+            return legalFormEntity.Id;
         }
 
-        public IEnumerable<LegalEntityGetDTO> GetLegalEntity()
+        public int Delete(int legalFormId)
         {
-            throw new NotImplementedException();
+            var legalFormEntity = _context.LegalForms.FirstOrDefault(x => x.Id == legalFormId);
+            if (legalFormEntity != null)
+            {
+                _context.Remove(legalFormEntity);
+                _context.SaveChanges();
+                return legalFormEntity.Id;
+            }
+            return -1;
         }
 
-        public int PutLegalForm(LegalFormPostDTO legalFormPutDTO)
+        public IEnumerable<LegalFormGetDTO> GetElemetsList()
         {
-            throw new NotImplementedException();
+            var legalFormEntity = _context.LegalForms.Select(x => _mapper.Map<LegalFormGetDTO>(x));
+            return legalFormEntity;
+        }
+
+        public int Update(LegalFormGetDTO legalFormDto)
+        {
+            var legalFormEntity = _context.LegalForms.FirstOrDefault(x => x.Id == legalFormDto.Id);
+            if (legalFormEntity != null)
+            {
+                legalFormEntity.LegalFormName = legalFormDto.LegalFormName;
+                legalFormEntity.Description = legalFormDto.Description;                
+
+                _context.SaveChanges();
+                return legalFormEntity.Id;
+            }
+            return -1;
         }
     }
 }
