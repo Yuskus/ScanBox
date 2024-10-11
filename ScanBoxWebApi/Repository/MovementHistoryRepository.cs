c31a803b5cd05bcd1a7e6bd531fad47f7eca1873
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-10-11 13:01:46 +0300

    Modified MovementHistoryRepository

--- ScanBoxWebApi/Repository/MovementHistoryRepository.cs
+++ ScanBoxWebApi/Repository/MovementHistoryRepository.cs
@@ -1,29 +1,65 @@
-﻿using DatabaseModel.DTO.GetDTO;
+﻿using AutoMapper;
+using DatabaseModel;
+using DatabaseModel.Context;
+using DatabaseModel.DTO.GetDTO;
 using DatabaseModel.DTO.PostDTO;
 using ScanBoxWebApi.Abstractions;
 
 namespace ScanBoxWebApi.Repository
 {
-    public class MovementHistoryRepository : IMovementHistoryRepository
+    public class MovementHistoryRepository : ICrudMethodRepository<MovementHistoryGetDTO, MovementHistoryPostDTO>
     {
-        public int AddMovementHistory(MovementHistoryPostDTO movementHistoryPostDTO)
+        public readonly ScanBoxDbContext _context;
+        public readonly IMapper _mapper;
+
+        public MovementHistoryRepository(ScanBoxDbContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _mapper = mapper;
         }
 
-        public int Delete(int Id)
+        public int Create(MovementHistoryPostDTO movementHistoryDto)
         {
-            throw new NotImplementedException();
+            var movementHistoryEntity = _context.MovementHistory.FirstOrDefault(x => x.DocumentId == movementHistoryDto.DocumentId);
+            if (movementHistoryEntity is null)
+            {
+                movementHistoryEntity = _mapper.Map<MovementHistoryEntity>(movementHistoryDto);
+                _context.Add(movementHistoryEntity);
+                _context.SaveChanges();
+            }
+            return movementHistoryEntity.Id;
         }
 
-        public IEnumerable<MovementHistoryGetDTO> GetMovementHistory()
+        public int Delete(int movementHistoryId)
         {
-            throw new NotImplementedException();
+            var movementHistoryEntity = _context.MovementHistory.FirstOrDefault(x => x.Id == movementHistoryId);
+            if (movementHistoryEntity is not null)
+            {
+                _context.Remove(movementHistoryEntity);
+                _context.SaveChanges();
+                return movementHistoryEntity.Id;
+            }
+            return -1;
         }
 
-        public int PutMovementHistory(MovementHistoryPostDTO movementHistoryPuttDTO)
+        public IEnumerable<MovementHistoryGetDTO> GetElemetsList()
         {
-            throw new NotImplementedException();
+            var movementHistoryEntity = _context.MovementHistory.Select(x => _mapper.Map<MovementHistoryGetDTO>(x));
+            return movementHistoryEntity;
+        }
+
+        public int Update(MovementHistoryGetDTO movementHistoryDto)
+        {
+            var movementHistoryEntity = _context.MovementHistory.FirstOrDefault(x => x.Id == movementHistoryDto.Id);
+            if (movementHistoryEntity is not null)
+            {
+                movementHistoryEntity.DocumentId = movementHistoryDto.DocumentId;
+                movementHistoryEntity.ProductUnitId = movementHistoryDto.ProductUnitId;
+
+                _context.SaveChanges();
+                return movementHistoryEntity.Id;
+            }
+            return -1;
         }
     }
 }
