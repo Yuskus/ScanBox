2535036db0b9580f5c33958f489b4c69f0a48fcb
Yuriy <yuriy_vm@inbox.ru>
2024-10-13 14:11:17 +0300

    Added BuyerController

---
+++ ScanBoxWebApi/Controllers/BuyerController.cs
@@ -0,0 +1,21 @@
+﻿using DatabaseModel.DTO.GetDTO;
+using DatabaseModel.DTO.PostDTO;
+using Microsoft.AspNetCore.Mvc;
+using ScanBoxWebApi.Abstractions;
+
+namespace ScanBoxWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BuyerController(ICrudMethodRepository<BuyerGetDTO, BuyerPostDTO> buyerRepository) : ControllerBase
+    {
+        private readonly ICrudMethodRepository<BuyerGetDTO, BuyerPostDTO> _buyerRepository = buyerRepository;
+
+        [HttpPost(template: "add_bueyr")]
+        public ActionResult AddBueyr(BuyerPostDTO buyerDTO)
+        {
+            var result = _buyerRepository.Create(buyerDTO);
+            return Ok(result);
+        }
+    }
+}
