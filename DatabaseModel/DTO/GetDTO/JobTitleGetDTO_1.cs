c39f8bdd41bdf697a909a74db4c3ab4bca27ad20
Yuriy <yuriy_vm@inbox.ru>
2024-10-13 22:51:24 +0300

    bad save

---
+++ DatabaseModel/DTO/GetDTO/JobTitleGetDTO_1.cs
@@ -0,0 +1,11 @@
+﻿namespace DatabaseModel.DTO.GetDTO
+{
+    public class JobTitleGetDTO
+    {
+        public int Id { get; set; }
+        public int CounterpartyId { get; set; }
+        public required string Surname { get; set; }
+        public required string Name { get; set; }
+        public string? Patronymic { get; set; }
+    }
+}
---
+++ DatabaseModel/DTO/PostDTO/JobTitlePostDTO_1.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseModel.DTO.PostDTO
+{
+    public class JobTitlePostDTO
+    {
+        public int CounterpartyId { get; set; }
+        public required string Surname { get; set; }
+        public required string Name { get; set; }
+        public string? Patronymic { get; set; }
+    }
+}
