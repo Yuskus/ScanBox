3d367730d02889f81e3c0a714271fcc7f49bfee0
Yuriy <yuriy_vm@inbox.ru>
2024-10-13 13:35:27 +0300

    modified WarehouseEmployeeRepository

--- ScanBoxWebApi/Repository/WarehouseEmployeeRepository.cs
+++ ScanBoxWebApi/Repository/WarehouseEmployeeRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using DatabaseModel;
 using DatabaseModel.Context;
 using DatabaseModel.DTO.GetDTO;
 using DatabaseModel.DTO.PostDTO;
@@ -16,24 +17,58 @@ namespace ScanBoxWebApi.Repository
             _context = context;
             _mapper = mapper;
         }
-        public int Create(WarehouseEmployeePostDTO dto)
+        public int Create(WarehouseEmployeePostDTO warehouseEployeeDto)
         {
-            throw new NotImplementedException();
+            var warehouseEmployeeEntity = _context.WarehouseEmployees.FirstOrDefault(x => x.Phone == warehouseEployeeDto.Phone);
+
+            if (warehouseEmployeeEntity is null)
+            {
+                warehouseEmployeeEntity = _mapper.Map<WarehouseEmployeeEntity>(warehouseEployeeDto);
+                _context.Add(warehouseEmployeeEntity);
+                _context.SaveChanges();
+            }
+            return warehouseEmployeeEntity.Id;
         }
 
-        public int Delete(int Id)
+        public int Delete(int warehouseEployeeId)
         {
-            throw new NotImplementedException();
+            var warehouseEmployeeEntity = _context.WarehouseEmployees.FirstOrDefault(x => x.Id == warehouseEployeeId);
+
+            int result = -1;
+            if (warehouseEmployeeEntity is not null)
+            {
+                result = warehouseEmployeeEntity.Id;
+                _context.Remove(warehouseEmployeeEntity);
+                _context.SaveChanges();
+            }
+            return result;
         }
 
         public IEnumerable<WarehouseEmployeeGetDTO> GetElemetsList()
         {
-            throw new NotImplementedException();
+            var warehouseEmployeeEntity = _context.WarehouseEmployees.Select(x => _mapper.Map<WarehouseEmployeeGetDTO>(x));
+            return warehouseEmployeeEntity;
         }
 
-        public int Update(WarehouseEmployeeGetDTO dto)
+        public int Update(WarehouseEmployeeGetDTO warehouseEployeeDto)
         {
-            throw new NotImplementedException();
+            var warehouseEmployeeEntity = _context.WarehouseEmployees.FirstOrDefault(x => x.Id == warehouseEployeeDto.Id);
+
+            if (warehouseEmployeeEntity is not null)
+            {
+                warehouseEmployeeEntity.JobTitleId = warehouseEployeeDto.JobTitleId;
+                warehouseEmployeeEntity.Surname = warehouseEployeeDto.Surname;
+                warehouseEmployeeEntity.Name = warehouseEployeeDto.Name;
+                warehouseEmployeeEntity.Patronymic = warehouseEployeeDto.Patronymic;
+                warehouseEmployeeEntity.Birthday = warehouseEployeeDto.Birthday;
+                warehouseEmployeeEntity.HireDate = warehouseEployeeDto.HireDate;
+                warehouseEmployeeEntity.Address = warehouseEployeeDto.Address;
+                warehouseEmployeeEntity.Phone = warehouseEployeeDto.Phone;
+                    
+                _context.SaveChanges();
+                return warehouseEmployeeEntity.Id;
+            }
+            return -1;
         }
     }
 }
