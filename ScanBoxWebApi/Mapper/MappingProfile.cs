14968a507de0447dfe5b8c083198da9c7c84d6ca
Yuriy <yuriy_vm@inbox.ru>
2024-11-03 12:40:30 +0300

    moddified the MappingProfile

--- ScanBoxWebApi/Mapper/MappingProfile.cs
+++ ScanBoxWebApi/Mapper/MappingProfile.cs
@@ -18,44 +18,42 @@ namespace ScanBoxWebApi.Mapper
                 .ForMember(dest => dest.Password, opt => opt.Ignore());
 
             // заполнение базы
-            CreateMap<BuyerGetDTO, BuyerEntity>();
-            CreateMap<CounterpartyGetDTO, CounterpartyEntity>();
-            CreateMap<CounterpartyTypeGetDTO, CounterpartyTypeEntity>();
-            CreateMap<DocumentGetDTO, DocumentEntity>();
-            CreateMap<DocumentTypeGetDTO, DocumentTypeEntity>();
-            CreateMap<JobTitleGetDTO, JobTitleEntity>();
-            CreateMap<LegalEntityGetDTO, LegalEntityEntity>();
-            CreateMap<LegalFormGetDTO, LegalFormEntity>();
-            CreateMap<ManufacturerGetDTO, ManufacturerEntity>();
-            CreateMap<MovementHistoryGetDTO, MovementHistoryEntity>();
-            CreateMap<PricesGetDTO, PricesEntity>();
-            CreateMap<ProductCategoryGetDTO, ProductCategoryEntity>();
-            CreateMap<ProductTypeGetDTO, ProductTypeEntity>();
-            CreateMap<ProductUnitGetDTO, ProductUnitEntity>();
-            CreateMap<ShipmentGetDTO, ShipmentEntity>();
-            CreateMap<SupplierGetDTO, SupplierEntity>();
-            CreateMap<BuyerGetDTO, BuyerEntity>();
-            CreateMap<WarehouseEmployeeGetDTO, WarehouseEmployeeEntity>();
+            CreateMap<BuyerPostDTO, BuyerEntity>();
+            CreateMap<CounterpartyPostDTO, CounterpartyEntity>();
+            CreateMap<CounterpartyTypePostDTO, CounterpartyTypeEntity>();
+            CreateMap<DocumentPostDTO, DocumentEntity>();
+            CreateMap<DocumentTypePostDTO, DocumentTypeEntity>();
+            CreateMap<JobTitlePostDTO, JobTitleEntity>();
+            CreateMap<LegalEntityPostDTO, LegalEntityEntity>();
+            CreateMap<LegalFormPostDTO, LegalFormEntity>();
+            CreateMap<ManufacturerPostDTO, ManufacturerEntity>();
+            CreateMap<MovementHistoryPostDTO, MovementHistoryEntity>();
+            CreateMap<PricesPostDTO, PricesEntity>();
+            CreateMap<ProductCategoryPostDTO, ProductCategoryEntity>();
+            CreateMap<ProductTypePostDTO, ProductTypeEntity>();
+            CreateMap<ProductUnitPostDTO, ProductUnitEntity>();
+            CreateMap<ShipmentPostDTO, ShipmentEntity>();
+            CreateMap<SupplierPostDTO, SupplierEntity>();
+            CreateMap<WarehouseEmployeePostDTO, WarehouseEmployeeEntity>();
 
             // чтение из базы
-            CreateMap<BuyerEntity, BuyerPostDTO>();
-            CreateMap<CounterpartyEntity, CounterpartyPostDTO>();
-            CreateMap<CounterpartyTypeEntity, CounterpartyTypePostDTO>();
-            CreateMap<DocumentEntity, DocumentPostDTO>();
-            CreateMap<DocumentTypeEntity, DocumentTypePostDTO>();
-            CreateMap<JobTitleEntity, JobTitlePostDTO>();
-            CreateMap<LegalEntityEntity, LegalEntityPostDTO>();
-            CreateMap<LegalFormEntity, LegalFormPostDTO>();
-            CreateMap<ManufacturerEntity, ManufacturerPostDTO>();
-            CreateMap<MovementHistoryEntity, MovementHistoryPostDTO>();
-            CreateMap<PricesEntity, PricesPostDTO>();
-            CreateMap<ProductCategoryEntity, ProductCategoryPostDTO>();
-            CreateMap<ProductTypeEntity, ProductTypePostDTO>();
-            CreateMap<ProductUnitEntity, ProductUnitPostDTO>();
-            CreateMap<ShipmentEntity, ShipmentPostDTO>();
-            CreateMap<SupplierEntity, SupplierPostDTO>();
-            CreateMap<BuyerEntity, BuyerPostDTO>();
-            CreateMap<WarehouseEmployeeEntity, WarehouseEmployeePostDTO>();
+            CreateMap<BuyerEntity, BuyerGetDTO>();
+            CreateMap<CounterpartyEntity, CounterpartyGetDTO>();
+            CreateMap<CounterpartyTypeEntity, CounterpartyTypeGetDTO>();
+            CreateMap<DocumentEntity, DocumentGetDTO>();
+            CreateMap<DocumentTypeEntity, DocumentTypeGetDTO>();
+            CreateMap<JobTitleEntity, JobTitleGetDTO>();
+            CreateMap<LegalEntityEntity, LegalEntityGetDTO>();
+            CreateMap<LegalFormEntity, LegalFormGetDTO>();
+            CreateMap<ManufacturerEntity, ManufacturerGetDTO>();
+            CreateMap<MovementHistoryEntity, MovementHistoryGetDTO>();
+            CreateMap<PricesEntity, PricesGetDTO>();
+            CreateMap<ProductCategoryEntity, ProductCategoryGetDTO>();
+            CreateMap<ProductTypeEntity, ProductTypeGetDTO>();
+            CreateMap<ProductUnitEntity, ProductUnitGetDTO>();
+            CreateMap<ShipmentEntity, ShipmentGetDTO>();
+            CreateMap<SupplierEntity, SupplierGetDTO>();
+            CreateMap<WarehouseEmployeeEntity, WarehouseEmployeeGetDTO>();
 
             // для сверки отгрузок, на всякий
             CreateMap<ShipmentPostDTO, MovementHistoryPostDTO>().ReverseMap();
