8e8d2a21944a45b6d74c8f579d1f7d2b508fef0c
Yuskus <yuskus@yandex.ru>
2024-11-04 00:06:41 +0300

    migration NullableFinalPrice added

---
+++ DatabaseModel/Migrations/20241103205636_NullableFinalPrice.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableFinalPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "realization_price",
+                table: "product_units_table",
+                type: "numeric(10,2)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "numeric(10,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "realization_price",
+                table: "product_units_table",
+                type: "numeric(10,2)",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "numeric(10,2)",
+                oldNullable: true);
+        }
+    }
+}
